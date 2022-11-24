b8bdc1c5f1a44784527ff126621428fab65ba049
aigerimbakh <milashkalovec@gmail.com>
2022-11-24 20:23:54 +0300

    git init

---
+++ src/main/java/kz/narxoz/shablonprgsrs/ShablonprgsrsApplication.java
@@ -0,0 +1,13 @@
+package kz.narxoz.shablonprgsrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShablonprgsrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShablonprgsrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/narxoz/shablonprgsrs/ShablonprgsrsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.shablonprgsrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShablonprgsrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
